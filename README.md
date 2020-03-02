05befa7e1d3a8786eec87c43ffaff6dc7d7515c0
kawinie <pechetrk@oregonstate.edu>
2020-03-01 17:59:49 -0800

    Updated README

--- README.md
+++ README.md
@@ -16,92 +16,90 @@ Pandathon supports the following core features
 - [ ] Function decaration and evaluation
 - [ ] First class functions
 
-### Assigning variables
+### Binding a Variable
 
 `Set` command is used to assign a value to variable. The following example binds "x" to integer 5.
 
 ```haskell
-Set "x" (I 5) // x = 5
+Set "x" (I 5) --> 5
 ```
 
 The variable can be reassigned to a different type the same way. Pandathon supports `I Int`, `F Float`, `B Bool`, `Str String`.
 
 ```haskell
-Set "x" (F 5)     // reassigning x to floating point number 5.0`
-Set "x" (Str "5") // x is now a string
+Set "x" (F 5)     -- reassigning x to floating point number 5.0`
+Set "x" (Str "5") -- x is now a string
 ```
 
-### Getting value
+### Getting Value
 
-`Get` command is used to obtain the value bounded to a variable.
+`Get` command is used to retrieve the value bounded to a variable.
 
 ```haskell
-Get "x" // -> Str "5"
+Get "x" --> Str "5"
 ```
 
-### The rest 
+### Basic Arithmetics
+
+Pandathon automatically performs implicit conversion between types if possible. This means that you can add Int and Float together or vice versa seemlessly.
 
 ```haskell
-Add (value) (value)
+Add (I 5) (I 10) --> 15
+Sub (F 5) (I 10) --> -15.0
+Mul (I 5) (F 10) --> 50.0
+Div (F 5) (F 10) --> 0.5
 ```
 
-This statement will add the two values together. Sub, Mul, Div, can also be used.
+### Comparisons
 
->`Lte (value) (value)`
+```haskell
+Set "x" (I 10)
+Lt (Get "x") (F 10)  --> False
+Lte (Get "x") (F 10) --> True
+Gt (Get "x") (F 10)  --> False
+Gte (Get "x") (F 10) --> True
+Eq (Get "x") (F 10)  --> True
+```
 
-This checks if the first value is less than or equal to the second. Gte, Lt, Gt, can also be used for their respective comparisons.
-
->`Do [statements]`
+### Control Flow
 
 This allows a user to continue within a while loop or an if-then block using a series of further programatic statements.
 
->`While (comparison) (Do [statements])`
+```haskell
+Begin [statements]
+```
 
 This allows a user to create a while loop with a comparison of values in the parentheses to loop with a block of code. This is followed by a Do block.
 
->`If (condition) (Do [statements]) (Do [statements])`
+```haskell
+While (condition) [statements]
+```
 
 This allows a user to enter an if-then block that starts in on the first Do block if the condition is met otherewise, it moves to the second Do block.
 
-For now, programs can be written by defining it by a name and then proceding to write it in brackets. For example:
-
 ```haskell
-test = [  
-    Set "x" (I 5),  
-    Dec "x"  
-]
+If (condition) [statements] [statements]
 ```
 
-Then, execute the program by calling `panda test`
+For now, programs can be written by defining it by a name and then proceding to write it in brackets. Then, execute the program by calling `panda cubs`:
 
-Which outputs:
->fromList [("x", I 4)]
+```haskell
+-- Fibbonaci:
+cubs = [
+        Set "a" (I 0),
+        Set "b" (I 1),
+        Set "count" (I 0),
+        While (Lt (Get "count") (I 10)) [
+            Set "c" (Add (Get "a") (Get "b")),
+            Set "a" (Get "b"),
+            Set "b" (Get "c"),
+            Inc "count"
+        ]
+    ]
 
-as x was decremented by one.
-
-There are more good and thorough examples of how to use the language inside of main.hs
-
-They can be ran with:
-
-Good examples:
->panda cubs
-
-expected output: 
-
->fromList [("count",I 6),("s",Str "Panda Panda 999.0"),("x",F 13.0)]
-
->panda cubs2
-
-expected output: 
-
->fromList [("count",I 9),("x",F 9.765625)]
-
-Bad examples:  
-These must be uncommented in main.hs. Will cause errors
-
->panda cubs3
-
->panda cubs4
+main = do
+    print (panda cubs) --> fromList [("a",I 55),("b",I 89),("c",I 89),("count",I 10)]
+```
 
 ## Authors
 
--- main.hs
+++ main.hs
@@ -8,8 +8,8 @@ import qualified Data.Map as Map
 
 --
 -- ────────────────────────────────────────────────────────────────────── II ──────────
---   :::::: A B S T R A C T   S Y N T A X : :  :   :    :     :        :          :
--- ────────────────────────────────────────────────────────────────────────────────
+--   :::::: A B S T R A Call T   S Y N T A X : :  :   :    :     :        :          :
+-- ──────────────────────────────────────────────────────────────────────────────── 
 --
 data Expr   = Get Var
             | I Int      -- Int 
@@ -17,7 +17,6 @@ data Expr   = Get Var
             | B Bool     -- Bool  (sugar for Int 0 and 1)
             | Str String -- String 
             | Cat Expr Expr
-            | Def String [String] Block
             | Add Expr Expr
             | Sub Expr Expr
             | Mul Expr Expr
@@ -37,11 +36,16 @@ data Test   = Eq  Expr Expr
             | Gte Expr Expr
     deriving (Show, Eq)
 
-data Statement  = If Test Block Block
-                | Set Var Expr
+data Statement  = Set Var Expr
                 | Inc Var
                 | Dec Var
+                | AddTo Var Expr
+                | SubTo Var Expr
+                | MulTo Var Expr
+                | DivTo Var Expr
+                | If Test Block Block
                 | While Test Block
+                | Func String [String] Block
                 | Call String [String]
                 | Begin Block
                 | End 
@@ -58,8 +62,9 @@ type Stack = [Env]
 --   :::::: V A L U A T I O N   F U N C T I O N S : :  :   :    :     :        :          :
 -- ────────────────────────────────────────────────────────────────────────────────────────
 --
-
--- ─── RECEIVE THE LITERAL VALUE OF THE EXPRESSION FROM THE ENVIRONMENT ───────────
+-- ────────────────────────────────────────────────────────────────────────────────
+-- ─── RETREIVE THE LITERAL VALUE OF THE EXPRESSION FROM THE ENVIRONMENT ──────────
+-- ────────────────────────────────────────────────────────────────────────────────
 get :: Env -> Var -> Expr
 get env v
     | Nothing <- value = error ("(ERROR) Variable " ++ show v ++ " doesn't exist") 
@@ -67,7 +72,9 @@ get env v
     where value = Map.lookup v env 
 
 
+-- ────────────────────────────────────────────────────────────────────────────────
 -- ─── UPDATE THE VALUE IF EXISTS OR INSERT A NEW ONE OTHERWISE ───────────────────
+-- ────────────────────────────────────────────────────────────────────────────────
 set :: Env -> Var -> Expr -> Env
 set env v expr
     | Just _  <- old = Map.adjust (\_ -> new) v env
@@ -85,7 +92,9 @@ op (F a, F b) _ o = F (o a b)
 op _ _ _ = error "(ERROR) Operation cannot be performed"
 
 
--- ─── VAL: GET LITERAL VALUE OF THE EXPRESSION ───────────────────────────────────────
+-- ────────────────────────────────────────────────────────────────────────────────
+-- ─── GET LITERAL VALUE OF THE EXPRESSION ───────────────────────────────────
+-- ────────────────────────────────────────────────────────────────────────────────
 val :: Env -> Expr -> Expr
 val env (Get v)   = get env v
 val env (B True)  = I 1
@@ -104,16 +113,18 @@ val env (Div e1 e2)
     -- If one of two expressions is a string, convert the other to a string using show()
 val env (Cat e1 e2)
     | (Str s1, Str s2) <- result = Str(s1 ++ s2)
-    | (Str s, I a)   <- result = Str(s ++ show a)
-    | (I a, Str s)   <- result = Str(show a ++ s)
-    | (Str s, F a)   <- result = Str(s ++ show a)
-    | (F a, Str s)   <- result = Str(show a ++ s)
+    | (Str s, I a)     <- result = Str(s ++ show a)
+    | (I a, Str s)     <- result = Str(show a ++ s)
+    | (Str s, F a)     <- result = Str(s ++ show a)
+    | (F a, Str s)     <- result = Str(show a ++ s)
     where result = (val env e1, val env e2)
 
 val env e = e
 
 
+-- ────────────────────────────────────────────────────────────────────────────────
 -- ─── VALUATION FUNCTION FOR TEST ────────────────────────────────────────────────
+-- ────────────────────────────────────────────────────────────────────────────────
 test :: Env -> Test -> Bool
 test env (Eq e1 e2)  = val env e1 == val env e2
 test env (Lt e1 e2)  = val env e1 <  val env e2
@@ -122,29 +133,40 @@ test env (Gt e1 e2)  = val env e1 >  val env e2
 test env (Gte e1 e2) = val env e1 >= val env e2
 
 
--- ─── VALUATION FUNCTION FOR STATEMENT ────────────────────────────────────────────
+-- ────────────────────────────────────────────────────────────────────────────────
+-- ─── VALUATION FUNCTION FOR STATEMENT ───────────────────────────────────────────
+-- ────────────────────────────────────────────────────────────────────────────────
 stmt :: Env -> Statement -> Env
 stmt env (End) = env
 stmt env (If t ss1 ss2) = if test env t then runBlock env ss1 else runBlock env ss2
-stmt env (While t ss)  = if test env t then stmt (runBlock env ss) (While t ss) else env
-stmt env (Set v expr) = set env v expr
-stmt env (Dec v) = set env v (Sub (get env v) (I 1))
-stmt env (Inc v) = set env v (Add (get env v) (I 1))
-stmt env (Begin ss) = runBlock env ss
+stmt env (While t ss)   = if test env t then stmt (runBlock env ss) (While t ss) else env
+stmt env (Set v expr)   = set env v expr
+stmt env (Begin ss)     = runBlock env ss -- Enter a block
+
+-- ─── SUGAR ──────────────────────────────────────────────────────────────────────
+stmt env (AddTo v expr) = set env v (Add (get env v) (expr))
+stmt env (SubTo v expr) = set env v (Sub (get env v) (expr))
+stmt env (MulTo v expr) = set env v (Mul (get env v) (expr))
+stmt env (DivTo v expr) = set env v (Div (get env v) (expr))
+stmt env (Dec v) = stmt env (SubTo v (I 1))
+stmt env (Inc v) = stmt env (AddTo v (I 1))
 
 
+-- ────────────────────────────────────────────────────────────────────────────────
 -- ─── VALUATION FUNCTION FOR A SERIES OF STATEMENTS ──────────────────────────────
+-- ────────────────────────────────────────────────────────────────────────────────
 -- TODO: remove the stackframe when exiting the block
 -- TODO: push a stackframe on to the stack
 runBlock :: Env -> [Statement] -> Env
-runBlock env [] = env   
+runBlock env [] = env   -- Exit a block
 runBlock env (s:ss) = runBlock (stmt env s) ss
 
 
+-- ────────────────────────────────────────────────────────────────────────────────
 -- ─── VALUATION FUNCTION FOR THE PROG WITH INITIALLY EMPTY ENVIRONMENT ───────────
+-- ────────────────────────────────────────────────────────────────────────────────
 panda :: [Statement] -> Env
 panda = runBlock Map.empty
-
 
 -- Fibbonaci:
 cubs = [
@@ -158,6 +180,10 @@ cubs = [
             Inc "count"
         ]
     ]
+
+
+main = do 
+    print (panda cubs)
      
 -- *Main> panda cubs
 -- fromList [("count",I 6),("s",Str "Panda Panda 999.0"),("x",F 13.0)]
